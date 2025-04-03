20a7bedb402d518ce9f1368e850d7116f47653e1
AlphonseMehounme <alphonsemehounme9@gmail.com>
2025-04-03 13:29:47 +0100

    create wallet and multisig address

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,5 @@
 # Create a native segwit address and get the public key from the address.
+segwitaddress=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32")
+addressinfo=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getaddressinfo $segwitaddress)
+pubkey=$(echo $addressinfo | jq -r '.pubkey')
+echo $pubkey
